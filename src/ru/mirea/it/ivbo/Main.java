0759b76522f1748eb3d4b97d4324b227cc14ebd5
makho <1@>
2023-09-02 12:02:19 +0300

    Initial

---
+++ src/ru/mirea/it/ivbo/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.it.ivbo;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("1", "2");
+        System.out.println(user.getPassword());
+    }
+}
