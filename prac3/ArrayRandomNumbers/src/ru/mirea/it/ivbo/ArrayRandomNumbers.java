049b4146e7e53bc4178ae7bd96a4a9fc41e70951
makho <1@>
2023-09-10 13:24:50 +0300

    P3

--- prac3/ArrayRandomNumbers/src/ru/mirea/it/ivbo/ArrayRandomNumbers.java
+++ prac3/ArrayRandomNumbers/src/ru/mirea/it/ivbo/ArrayRandomNumbers.java
@@ -1,7 +1,27 @@
 package ru.mirea.it.ivbo;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Random;
+
 public class ArrayRandomNumbers {
     public static void main(String[] args) {
-
+        DecimalFormat decimalFormat = new DecimalFormat("#.###");
+        Random random = new Random();
+        double[] array = new double[10];
+        for (int i = 0; i < array.length; i++)
+            array[i] = random.nextDouble() * 100;
+        System.out.println("Первый массив, созданный с помощью класса Random:");
+        for (double v : array) System.out.print(decimalFormat.format(v) + "\t");
+        System.out.println("\nОтсортированный первый массив:");
+        Arrays.sort(array);
+        for (double v : array) System.out.print(decimalFormat.format(v) + "\t");
+        System.out.println("\n\nВторой массив, созданный с помощью метода random() класса Math:");
+        for (int i = 0; i < array.length; i++)
+            array[i] = Math.random() * 100;
+        for (double v : array) System.out.print(decimalFormat.format(v) + "\t");
+        System.out.println("\nОтсортированный Второй массив:");
+        Arrays.sort(array);
+        for (double v : array) System.out.print(decimalFormat.format(v) + "\t");
     }
 }
