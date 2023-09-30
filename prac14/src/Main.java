f96925a3cd3c574625f3c61cc0306748c976f654
makho <1@>
2023-09-30 11:05:49 +0300

    P13

--- prac14/src/Main.java
+++ prac14/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+        Pattern p2 = Pattern.compile("abcdefghijklmnopqrstuv18340");
+        Matcher m2 = p2.matcher(s);
+        System.out.println(s + ": " + m2.matches());
+
+        Pattern p4 = Pattern.compile("\\+\\s*\\d+");
+        s = scanner.nextLine();
+        Matcher m4 = p4.matcher(s);
+        System.out.println(s + ": " + m4.find());
+
+        Pattern p6 = Pattern.compile("(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*");
+        s = scanner.nextLine();
+        Matcher m6 = p6.matcher(s);
+        System.out.println(s + ": " + m6.matches());
+
     }
 }
