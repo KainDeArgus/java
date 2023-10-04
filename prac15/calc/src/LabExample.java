0f5cbdc62816cd6258ed3e74c24ac84fd0d43644
makho <1@>
2023-10-04 22:05:58 +0300

    P15

---
+++ prac15/calc/src/LabExample.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class LabExample extends JFrame {
+    JTextField jta1 = new JTextField(10);
+    JTextField jta2 = new JTextField(10);
+    JButton button_addition = new JButton("+");
+    JButton button_substraction = new JButton("-");
+    JButton button_division = new JButton("/");
+    JButton button_multyplication = new JButton("*");
+    Font fnt = new Font("Times new roman", Font.BOLD, 20);
+
+    //начало конструктора класса LabExample
+    LabExample() {
+        super("Example");
+        setLayout(new FlowLayout());
+        setSize(200, 150);
+// создаем надпись 1
+        add(new JLabel("1st Number"));
+// добавляем текстовое поле jta1
+        add(jta1);
+// создаем надпись 2
+        add(new JLabel("2nd Number"));
+// добавляем текстовое поле jta2
+        add(jta2);
+// добавляем кнопку
+        add(button_addition);
+        // добавляем реализацию actionPerformed
+        button_addition.addActionListener(e -> {
+            try {
+//переменная для хранения ввода в текстовое поле 1
+                double x1 = Double.parseDouble(jta1.getText().trim());
+//переменная для хранения ввода в текстовое поле 2
+                double x2 = Double.parseDouble(jta2.getText().trim());
+//создаем всплывающее окно INFORMATION_MESSAGE
+                JOptionPane.showMessageDialog(null, "Result = " + (x1 + x2), "Alert", JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception error) {
+//создаем всплывающее окно ERROR_MESSAGE
+                JOptionPane.showMessageDialog(null, "Error in Numbers !", "alert", JOptionPane.ERROR_MESSAGE);
+            }
+        });// конец button.addActionListener()
+
+        add(button_substraction);
+        // добавляем реализацию actionPerformed
+        button_substraction.addActionListener(e -> {
+            try {
+//переменная для хранения ввода в текстовое поле 1
+                double x1 = Double.parseDouble(jta1.getText().trim());
+//переменная для хранения ввода в текстовое поле 2
+                double x2 = Double.parseDouble(jta2.getText().trim());
+//создаем всплывающее окно INFORMATION_MESSAGE
+                JOptionPane.showMessageDialog(null, "Result = " + (x1 - x2), "Alert", JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception error) {
+//создаем всплывающее окно ERROR_MESSAGE
+                JOptionPane.showMessageDialog(null, "Error in Numbers !", "alert", JOptionPane.ERROR_MESSAGE);
+            }
+        });// конец button.addActionListener()
+        add(button_division);
+        // добавляем реализацию actionPerformed
+        button_division.addActionListener(e -> {
+            try {
+//переменная для хранения ввода в текстовое поле 1
+                double x1 = Double.parseDouble(jta1.getText().trim());
+//переменная для хранения ввода в текстовое поле 2
+                double x2 = Double.parseDouble(jta2.getText().trim());
+//создаем всплывающее окно INFORMATION_MESSAGE
+                JOptionPane.showMessageDialog(null, "Result = " + (x1 / x2), "Alert", JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception error) {
+//создаем всплывающее окно ERROR_MESSAGE
+                JOptionPane.showMessageDialog(null, "Error in Numbers !", "alert", JOptionPane.ERROR_MESSAGE);
+            }
+        });// конец button.addActionListener()
+        add(button_multyplication);
+        // добавляем реализацию actionPerformed
+        button_multyplication.addActionListener(e -> {
+            try {
+//переменная для хранения ввода в текстовое поле 1
+                double x1 = Double.parseDouble(jta1.getText().trim());
+//переменная для хранения ввода в текстовое поле 2
+                double x2 = Double.parseDouble(jta2.getText().trim());
+//создаем всплывающее окно INFORMATION_MESSAGE
+                JOptionPane.showMessageDialog(null, "Result = " + (x1 * x2), "Alert", JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception error) {
+//создаем всплывающее окно ERROR_MESSAGE
+                JOptionPane.showMessageDialog(null, "Error in Numbers !", "alert", JOptionPane.ERROR_MESSAGE);
+            }
+        });// конец button.addActionListener()
+        setVisible(true);
+    } // конец конструктора
+}
---
+++ prac15/second_task/src/TestJComboBox.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TestJComboBox{
+    public static void test(){
+        JFrame frame = new JFrame("TestJComboBox");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setLocationRelativeTo(null);
+
+        String[] countries = {
+                "Australia",
+                "China",
+                "England",
+                "Russia"
+        };
+
+        JPanel info = new JPanel();
+        JLabel info_label = new JLabel();
+        GridLayout layoutMain = new GridLayout(2, 1);
+        info.add(info_label);
+        JComboBox<String> jComboBox = new JComboBox<String>(countries);
+        jComboBox.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                JComboBox<String> combo = (JComboBox<String>) event.getSource();
+                String selectedBook = (String) combo.getSelectedItem();
+
+                if (selectedBook.equals("Australia")) {
+                    info_label.setText("The capital of Australia is Canberra");
+                } else if (selectedBook.equals("China")) {
+                    info_label.setText("The capital of China is Beijing!");
+                } else if (selectedBook.equals("England")) {
+                    info_label.setText("The capital of England is London");
+                } else if (selectedBook.equals("Russia")) {
+                    info_label.setText("The capital of Russia is Moscow");
+                }
+
+            }
+        });
+
+        JPanel main = new JPanel();
+        main.add(jComboBox);
+        main.add(info);
+        main.setLayout(layoutMain);
+        frame.add(main);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+
+    }
+}
